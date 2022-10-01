e6a2dbd5709c5eff38ff5359604834ae48335e26
Ritesh Kumar <52500946+Ritesh-FSD@users.noreply.github.com>
2022-10-01 11:12:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,33 @@ menu.onclick = () =>{
   });
 });
 
+
+// Premium courses
+
+
+
+
+
+
+
+
+// Testimonial-section
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+
+
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+
+  });
+});
